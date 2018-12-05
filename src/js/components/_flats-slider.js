7314064485289cc28941934ffe6963a9e81e0a17
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-12-05 23:23:48 +0200

    mixins

--- src/js/components/_flats-slider.js
+++ src/js/components/_flats-slider.js
@@ -2,18 +2,28 @@ import slick from 'slick-carousel';
 
 $(document).ready(function() {
   /* FLATS SLIDER */
-  var slider = $(".js-flats-slider");
+  var slider = $('.js-flats-slider');
 
-  $(slider).on("init", function(event, slick) {
-    $(slider).css({ opacity: "1", visibility: "visible" });
+  $(slider).on('init', function(event, slick) {
+    $(slider).css({ opacity: '1', visibility: 'visible' });
   });
 
   $(slider).slick({
     slidesToShow: 1,
     centerMode: true,
     arrows: false,
-    speed: 1000
-    // fade: true
+    speed: 1000,
+    fade: true
     // cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  });
+
+  var thumbs = $('.js-flats-thumbs');
+  $(thumbs).slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: slider,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
   });
 });
